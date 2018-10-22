464742c1aac27477bd4c292b75ba94b6c443ee01
朱聪 <1902726060@qq.com>
2018-10-22 14:40:23 +0800

    test

--- Music163.py
+++ Music163.py
@@ -1,26 +1,84 @@
+# -*- coding: utf-8 -*-
+# @Author Cong.zhu  #
+
 import requests
 from bs4 import BeautifulSoup
 import re
+import redis
+import json
+class Music163:
 
+    def __init__(self):
+        self.r = redis.Redis(host='127.0.0.1', port=6379, decode_responses=True)
+        self.headers = {
+            "Referer":
+            "https://music.163.com/",
+            "Host":
+            "music.163.com",
+            "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
+            "Accept":
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+        }
+    #下载歌单页面
+    def DownLoadHtml(self,url):
+        urls = "https://music.163.com/playlist?id=2318367080"
+        music_html = requests.get(urls, headers=self.headers)
+        soup = BeautifulSoup(music_html.text, "lxml")
+        return soup
+    #获取歌单里面歌曲列表
+    def get_music_list(self,html):
+        #keyid=html.find("div",class_="n-cmt").get("data-tid")
+        mm = html.find("ul",class_='f-hide').find_all("li")
+        gedanname = html.find_all("link")[0].get('href')
+        self.r.hset("歌单",html.title.get_text(),gedanname)
+        for li in mm:
+            #print(li.find("a").get_text())
+            #self.r.hmset(li.find("a").get_text(),{'name':li.find("a").get_text(),'id':li.find("a").get('href')})
+            self.r.hset(html.title.get_text(),li.find("a").get_text(),li.find("a").get('href'))
 
-def get_music_html():
-    urls = "https://music.163.com/#/playlist?id=402924168"
-    headers = {
-        "Referer":
-        "https://music.163.com/",
-        "Host":
-        "music.163.com",
-        "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36",
-        "Accept":
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
-    }
-    music_html = requests.get(urls, headers=headers)
-    soup = BeautifulSoup(music_html.text, "lxml")
-    #results = soup.find_all('attrs={'id':'song-list-pre-cache'}')#.find_all('li')
-    results = soup.find_all('li')
-    print(soup)
+    #获取评论JSON数据
+    def GetComments(self,songid):
+        url='http://music.163.com/api/v1/resource/comments/R_SO_4_%s'%songid
+        commentsjoin=requests.get(url,headers=self.headers)
+        return commentsjoin.json() #[1]["content"]
+    
+    #获取热门评论
+    def Get_HotComments(self,data,songname):
+        for ent in data["hotComments"]:
+            print(ent["content"])
+            self.writefile(songname,ent["content"])
+        
+    #写入文本数据
+    def writefile(self,filename,content):
+        filename=self.validateTitle(filename)
+        path='E:\网易云音乐歌曲评论\%s.txt'%filename
+        with open(path,'a+',encoding='utf-8') as fileobject: #使用‘w’来提醒python用写入的方式打开
+            fileobject.writelines(content+"\n")
 
+    def RedisHelp(self):
+        return self.r.hgetall("歌单")
+
+    def validateTitle(self,title):
+        rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
+        new_title = re.sub(rstr, "_", title)  # 替换为下划线
+        return new_title
 
 if __name__ == "__main__":
-    get_music_html()
+    music=Music163()
+    # url=""
+    # html=music.DownLoadHtml(url)
+    # music.get_music_list(html)
+    #jsss=music.GetComments('516997458')
+    #music.Get_HotComments(jsss)
+    for key in music.RedisHelp():
+        print("*****************************************\n")
+        for kv in (music.r.hgetall(key).items()):
+            songname=kv[0]
+            songid=kv[1].split("=")[1]
+            data=music.GetComments(songid)
+            music.Get_HotComments(data,songname)
+
+            
+            
+   
--- TouTiao.py
+++ TouTiao.py
@@ -10,12 +10,11 @@ from urllib.parse import urlencode #将字段转换为url的参数
 
 from hashlib import md5 #转换md5的值
 
-import PySqlHelp as comm
+#import PySqlHelp as comm
 
 import time
-
-keyword = 'Python'
-
+import sys
+keyword = '耐克'
 #请求页面，获得json格式数据
 def get_page(offset):
 	#定义参数
@@ -28,11 +27,20 @@ def get_page(offset):
 		'cur_tab':'1',
 		'from':'search_tab',
 	}
+
+	headers={
+		'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
+		'referer':'https://www.toutiao.com/search/?keyword=%E8%80%90%E5%85%8B',#+urlencode(keyword),
+		'accept-language':'zh-CN,zh;q=0.9,en;q=0.8'
+	}
 	#请求连接
+	#https://www.toutiao.com/search_content/
+	# ?offset=0&format=json&keyword=%E8%80%90%E5%85%8B&autoload=true&count=20&cur_tab=1&from=search_tab
 	url="https://www.toutiao.com/search_content/?"+urlencode(para)
+	print(url)
 	try:
 		#请求定义好的url
-		response=requests.get(url);
+		response=requests.get(url,headers=headers)	
 		#如果请求成功返回json
 		if response.status_code==200:
 			return response.json()
@@ -44,14 +52,13 @@ def get_page(offset):
 #解析json对象里面的图片url和title
 def get_image(json):
 	if json.get('data'):
-		
 		#print (json.get('data'))
 		for dataitem in json.get('data'):
 			if dataitem.get('title')==None:
-					continue
+				continue
 			else:
 				title=dataitem.get('title')
-				images=dataitem.get('image_detail')
+				images=dataitem.get('image_list')
 				if images == None:
 					continue
 				else:
@@ -79,16 +86,17 @@ def save_image(item):
 		except OSError:
 			print(item.get('title'))
 	try:
-		response=requests.get(item.get('image'))
-		if response.status_code==200:
-			file_path='E:/Python/TouTiao/{0}/{1}.{2}'.format(validateTitle(item.get('title')),md5(response.content).hexdigest(),'jpg')
+		imageurl=item.get('image')
+		responseimage=requests.get('http:'+imageurl)
+		if responseimage.status_code==200:
+			file_path='E:/Python/TouTiao/{0}/{1}.{2}'.format(validateTitle(item.get('title')),md5(responseimage.content).hexdigest(),'jpg')
 			if not os.path.exists(file_path):
 				with open(file_path,'wb') as f:
-					f.write(response.content)
-					comm.InsertTouTiao('INSERT TouTiao VALUES ('"'{0}'"','"'{1}'"','"'{2}'"','"'{3}'"')'.format(validateTitle(item.get('title')),item.get('image'), time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),keyword))
+					f.write(responseimage.content)
+					#comm.InsertTouTiao('INSERT TouTiao VALUES ('"'{0}'"','"'{1}'"','"'{2}'"','"'{3}'"')'.format(validateTitle(item.get('title')),item.get('image'), time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),keyword))
 			else:
 				print('Already Downloaded',file_path)
-	except response.ConnectionError:
+	except requests.ConnectionError:
 		print('Failed to Save Image')
 
 
@@ -107,7 +115,7 @@ def validateTitle(title):
 
 #主函数
 def main(offset):
-	json=get_page(offset);
+	json=get_page(offset)
 	for item in get_image(json):
 		#print(item)
 		save_image(item)
@@ -119,11 +127,12 @@ GROUP_END = 20
 
 if __name__=='__main__':
 	#实例化进程类
-	pool=Pool()
-	#groups = ([x * 20 for x in range(GROUP_START, GROUP_END + 1)])
-	groups=('0')
+	# pool=Pool()
+	# #groups = ([x * 20 for x in range(GROUP_START, GROUP_END + 1)])
+	# groups=('0')
 
-	pool.map(main,groups)
-	pool.close()
-	pool.join()
+	# pool.map(main,groups)
+	# pool.close()
+	# pool.join()
+	main(0)
 
